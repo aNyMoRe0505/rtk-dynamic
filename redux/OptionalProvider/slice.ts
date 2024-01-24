079c3b805110643db0ad76f98d904747bea70886
Paul <anymore0505@gmail.com>
2024-01-24 15:39:52 +0800

    approach 4

--- redux/OptionalProvider/slice.ts
+++ redux/OptionalProvider/slice.ts
@@ -1,4 +1,8 @@
-import { createSlice, WithSlice } from '@reduxjs/toolkit';
+import type { Dispatch, ListenerEffect, WithSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+import rootReducer, { RootState } from '../reducers';
+import { startAppListening } from '../configureStore';
 
 export const initialState = {
   optionalCount: 0,
@@ -13,11 +17,30 @@ const slice = createSlice({
   },
 });
 
+export const withOptionalSlice = rootReducer.inject(slice);
+
 declare module '../reducers' {
   export interface LazyLoadedSlices extends WithSlice<typeof slice> {}
 }
 
 export const { optionalIncrement, optionalDecrement } = slice.actions;
+
+const onOptionalIncrementEffect: ListenerEffect<ReturnType<typeof optionalIncrement>, RootState, Dispatch> = () => {
+  console.log('onOptionalIncrementEffect');
+};
+
+const onOptionalDecrementEffect: ListenerEffect<ReturnType<typeof optionalDecrement>, RootState, Dispatch> = () => {
+  console.log('onOptionalDecrementEffect');
+};
+
+startAppListening({
+  actionCreator: optionalIncrement,
+  effect: onOptionalIncrementEffect,
+});
+startAppListening({
+  actionCreator: optionalDecrement,
+  effect: onOptionalDecrementEffect,
+});
 
 export default slice;
 
